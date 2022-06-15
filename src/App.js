d9d2a9101630845bdbe35afcea69f303dcdc81de
mahyarErshad <mahyar.ershad92@gmail.com>
2022-06-15 18:34:00 +0430

    added Header, Main, Footer for login page and Main css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-  <h1>Hello world</h1>
+      <Header />
+      <Main />
+      <Footer />
     </>
   );
 }
