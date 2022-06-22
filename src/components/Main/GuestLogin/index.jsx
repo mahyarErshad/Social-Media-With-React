6f45beb8caaa45d2560a45c97df048ec93355520
mahyarErshad <mahyar.ershad92@gmail.com>
2022-06-22 21:57:17 +0430

    installed axios

--- src/components/Main/GuestLogin/index.jsx
+++ src/components/Main/GuestLogin/index.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Container from "../Container";
 
 function Main() {
+  
+
   return (
     <Container wide={true} title = {"Social Media"}>
       <div className="row align-items-center">
@@ -10,7 +12,7 @@ function Main() {
           <p className="lead text-muted">Are you sick of short tweets and impersonal &ldquo;shared&rdquo; posts that are reminiscent of the late 90&rsquo;s email forwards? We believe getting back to actually writing is the key to enjoying the internet again.</p>
         </div>
         <div className="col-lg-5 pl-lg-5 pb-3 py-lg-5">
-          <form>
+          <form onSubmit={handleSumbit}>
             <div className="form-group">
               <label htmlFor="username-register" className="text-muted mb-1">
                 <small>Username</small>
